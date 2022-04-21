1ebfe05122adcabc49be491a82f624cf939a0254
MahmudJion <mahmudh.jion02@gmail.com>
2022-04-21 07:14:25 +0100

    update route

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get "/articles", to: "articles#index"
     get "/articles/:id", to: "articles#show"
 
-    resources :articles
+    resources :articles do
+    resources :comments
   end
